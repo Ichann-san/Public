2e6f786fcbea1b155000bbbd57451441bb9f7fa6
Maulana Ikhsan <maulana.ikhsan2018@gmail.com>
2025-09-15 15:03:59 +0700

    Commit

---
+++ Personal/Learn_lisa/sintaks.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int x=10, j=0;
+
+    //  awal    akhir  operasi
+    for(int i=0; i<x; i++){
+        //programnya
+    }
+
+    while(j<x){
+        //programnya
+        j++;
+    }
+
+    do{
+        //programnya
+        j++;
+    } while(j>x);
+
+
+return 0;
+}
